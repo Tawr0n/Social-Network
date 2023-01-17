e36b62a67e6a30a2d01c6ea20ae58d615a7ba5ef
Tawr0n <yaroslav.kit.it@gmail.com>
2023-01-17 18:57:12 +0200

    The basic site template (#11)

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+    return (
+        <header className={'header'}>
+            <div className="header__logo">
+                <img
+                     src="https://wowletsparty.com//image/cache/catalog/CATALOG%20_%202020/Harry%20Potter/551890%209%20IN-835x835.jpg"
+                     alt="logo"/>
+            </div>
+        </header>
+    )
+}
+
+export default Header
