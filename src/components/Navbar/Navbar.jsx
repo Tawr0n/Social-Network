973351b3ea75016d2770cfb1d4feedef605c7025
Tawr0n <yaroslav.kit.it@gmail.com>
2023-01-19 01:02:29 +0200

    NavLink (#20)

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,17 @@
 import s from './Navbar.module.css'
+import {NavLink} from "react-router-dom";
 
+const isActive = ({isActive}) => isActive ? s.link_active : s.link
 const Navbar = () => {
     return (
         <nav className={s.nav}>
-            <ul>
-                <li><a className={`${s.link} ${s.active}`} href="/Profile">Profile</a></li>
-                <li><a className={s.link} href="/Messages">Messages</a></li>
-                <li><a className={s.link} href="/News">News</a></li>
-                <li><a className={s.link} href="/Music">Music</a></li>
-                <li><a className={s.link} href="/Settings">Settings</a></li>
+            <ul className={s.nav__links}>
+                <li><NavLink className={isActive}
+                             to="/Profile">Profile</NavLink></li>
+                <li><NavLink className={isActive} to="/Messages">Messages</NavLink></li>
+                <li><NavLink className={isActive} to="/News">News</NavLink></li>
+                <li><NavLink className={isActive} to="/Music">Music</NavLink></li>
+                <li><NavLink className={isActive} to="/Settings">Settings</NavLink></li>
             </ul>
         </nav>
     )
