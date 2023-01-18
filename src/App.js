253284cc18757dcab5786371c59209e45ef2fd18
Tawr0n <yaroslav.kit.it@gmail.com>
2023-01-18 22:12:07 +0200

    Messages page (#18)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
+import Messages from "./components/Messages/Messages";
 
 function App() {
     return (
@@ -9,7 +10,11 @@ function App() {
             <div className="wrapper__container">
                 <Header/>
                 <Navbar/>
-                <Profile/>
+                <main className={'content'}>
+                    {/*<Profile/>*/}
+                    <Messages/>
+                </main>
+
             </div>
         </div>
     );
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
     return (
-        <main className={s.main}>
+        <div className={s.main}>
             <div className={s.main__imgBlock}>
                 <img
                     src="https://64.media.tumblr.com/e10123b95d3c4fdfc1e5a53beb0be5a7/588aba39c09f8322-a4/s1280x1920/6cc423179fb28e53335c536e5586bdd4543b68f1.jpg"
@@ -22,7 +22,7 @@ const Profile = () => {
                 </div>
             </div>
             <MyPosts/>
-        </main>
+        </div>
     )
 }
 
