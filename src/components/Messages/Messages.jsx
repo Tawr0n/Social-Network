ed6a307c8983657ac422f119afebb5181003b4e8
Tawr0n <yaroslav.kit.it@gmail.com>
2023-01-22 17:30:32 +0200

    onClick, ref (#31)

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -5,13 +5,24 @@ import Message from "./Message/Message";
 
 
 const Messages = ({messagesPage}) => {
-
+    const textareaMessage = React.createRef()
+    const sendMessage = () => {
+        const message = textareaMessage.current.value
+        alert(message)
+    }
     return (<section className={s.section}>
         <div className={s.dialogs}>
             {messagesPage.dialogs.map(d => <Dialog id={d.id} name={d.name} text={d.text}/>)}
         </div>
-        <div className={s.messages}>
-            {messagesPage.messages.map(m => <Message id={m.id} message={m.message}/>)}
+        <div className={s.chat}>
+            <div className={s.messages}>
+                {messagesPage.messages.map(m => <Message id={m.id} message={m.message}/>)}
+            </div>
+            <div className={s.chat__functional}>
+                <textarea ref={textareaMessage} className={s.chat__input}
+                          placeholder={'Написати повідомлення...'}/>
+                <button onClick={sendMessage} className={s.chat__button}>Надіслати</button>
+            </div>
         </div>
     </section>);
 };
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,16 +1,24 @@
+import React from "react";
 import s from './MyPosts.module.css'
 import Post from "./Post/Post";
 
 const MyPosts = ({posts}) => {
+
+    const textareaRef = React.createRef()
+    const addPost = () => {
+        const text = textareaRef.current.value
+        alert(text)
+    }
+
     return (
         <div className={`main__posts ${s.posts}`}>
             <h2 className={s.posts__title}>MyPosts</h2>
             <div>
-                 <textarea className={s.posts__input} placeholder={'Введіть текст...'} cols="20"
+                 <textarea ref={textareaRef} className={s.posts__input} placeholder={'Введіть текст...'} cols="20"
                            rows="3"/>
             </div>
             <div>
-                <button className={s.posts__button}>Відправити</button>
+                <button onClick={addPost} className={s.posts__button}>Відправити</button>
             </div>
             {posts.map(p => <Post id={p.id} message={p.message} likesCount={p.likesCount}/>)}
         </div>
