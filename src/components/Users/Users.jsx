89d04e3fc2b4817ef17ef581e37a893e9c902e9a
Tawr0n <yaroslav.kit.it@gmail.com>
2023-01-29 01:38:39 +0200

    Class Component (#53)

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,25 +3,27 @@ import s from './Users.module.css'
 import User from "./User/User";
 import axios from "axios";
 
-const Users = ({users, followToggle, setUsers}) => {
-    const getUsers = () => {
-        if (users.length === 0) {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-                setUsers(response.data.items)
-            })
-
+class Users extends React.Component {
+    constructor(props) {
+        super(props)
+        if (this.props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users')
+                .then(response => {
+                    this.props.setUsers(response.data.items)
+                })
         }
     }
 
-    return (
-        <section className={s.users}>
-            {
-                users.length
-                    ? users.map(u => <User key={u.id} followToggle={followToggle} {...u} />)
-                    : <button onClick={getUsers}>Get users</button>
-            }
-        </section>
-    );
-};
+    render() {
+        return (
+            <section className={s.users}>
+                {
+                    this.props.users.map(u => <User key={u.id}
+                                                    followToggle={this.props.followToggle} {...u} />)
+                }
+            </section>
+        )
+    }
+}
 
 export default Users;
